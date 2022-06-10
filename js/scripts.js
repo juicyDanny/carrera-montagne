3f7a4f8c19a0071be64519a9bfebe233d74f1c32
Jeremaz <jere.mazzetti@gmail.com>
2022-06-10 02:27:41 -0300

    Se agrega perloader

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,14 @@ var currentScrollPos = window.pageYOffset;
   }
   prevScrollpos = currentScrollPos;
 }
-window.onload = function(){ document.querySelector(".preloader").style.display = "none"; }
+
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
