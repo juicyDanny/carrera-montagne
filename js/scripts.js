b715794318fd75fc48649090e1ca3fea707e237c
Iván <ivanantoniord@gmail.com>
2022-06-22 23:25:49 -0300

    Se agrego el carruzel que iba a hacer fede y se modifico el css del index

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,33 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("categorias").style.display = "block";
 }
+
+//Para la vitriera slideshow en home
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Boton Next/previo
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// 
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slideshow-home");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
